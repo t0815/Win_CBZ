9418b5d8e45eee70186ae8ab81f2a913e652161f
t0815 <10379828+t0815@users.noreply.github.com>
2023-08-14 23:00:54 +0200

    make metadata view position center

--- MyCBZ/Forms/MetaDataForm.Designer.cs
+++ MyCBZ/Forms/MetaDataForm.Designer.cs
@@ -34,9 +34,8 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.ButtonCancel = new System.Windows.Forms.Button();
             this.SettingsGroup1Panel = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.metaDataView = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
             this.MetaDataTablePanel.SuspendLayout();
             this.HeaderPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -48,17 +47,19 @@
             this.MetaDataTablePanel.ColumnCount = 3;
             this.MetaDataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.462523F));
             this.MetaDataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 98.53748F));
-            this.MetaDataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 132F));
+            this.MetaDataTablePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 99F));
             this.MetaDataTablePanel.Controls.Add(this.HeaderPanel, 0, 0);
             this.MetaDataTablePanel.Controls.Add(this.ButtonCancel, 2, 2);
             this.MetaDataTablePanel.Controls.Add(this.SettingsGroup1Panel, 1, 1);
-            this.MetaDataTablePanel.Location = new System.Drawing.Point(1, 3);
+            this.MetaDataTablePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MetaDataTablePanel.Location = new System.Drawing.Point(0, 0);
+            this.MetaDataTablePanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MetaDataTablePanel.Name = "MetaDataTablePanel";
             this.MetaDataTablePanel.RowCount = 3;
-            this.MetaDataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.02235F));
-            this.MetaDataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75.97765F));
-            this.MetaDataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 62F));
-            this.MetaDataTablePanel.Size = new System.Drawing.Size(680, 326);
+            this.MetaDataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.41223F));
+            this.MetaDataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 86.58777F));
+            this.MetaDataTablePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.MetaDataTablePanel.Size = new System.Drawing.Size(655, 558);
             this.MetaDataTablePanel.TabIndex = 1;
             // 
             // HeaderPanel
@@ -67,26 +68,30 @@
             this.MetaDataTablePanel.SetColumnSpan(this.HeaderPanel, 3);
             this.HeaderPanel.Controls.Add(this.HeaderLabel);
             this.HeaderPanel.Controls.Add(this.pictureBox1);
-            this.HeaderPanel.Location = new System.Drawing.Point(3, 3);
+            this.HeaderPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HeaderPanel.Location = new System.Drawing.Point(2, 2);
+            this.HeaderPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.HeaderPanel.Name = "HeaderPanel";
-            this.HeaderPanel.Size = new System.Drawing.Size(674, 57);
+            this.HeaderPanel.Size = new System.Drawing.Size(651, 64);
             this.HeaderPanel.TabIndex = 0;
             // 
             // HeaderLabel
             // 
             this.HeaderLabel.AutoSize = true;
-            this.HeaderLabel.Location = new System.Drawing.Point(92, 24);
+            this.HeaderLabel.Location = new System.Drawing.Point(69, 20);
+            this.HeaderLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.HeaderLabel.Name = "HeaderLabel";
-            this.HeaderLabel.Size = new System.Drawing.Size(125, 16);
+            this.HeaderLabel.Size = new System.Drawing.Size(103, 13);
             this.HeaderLabel.TabIndex = 1;
             this.HeaderLabel.Text = "Metadata XML View";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Win_CBZ.Properties.Resources.info_dialog;
-            this.pictureBox1.Location = new System.Drawing.Point(18, 6);
+            this.pictureBox1.Location = new System.Drawing.Point(14, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(58, 55);
+            this.pictureBox1.Size = new System.Drawing.Size(44, 45);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -94,9 +99,10 @@
             // 
             this.ButtonCancel.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.ButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.ButtonCancel.Location = new System.Drawing.Point(558, 278);
+            this.ButtonCancel.Location = new System.Drawing.Point(564, 519);
+            this.ButtonCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ButtonCancel.Name = "ButtonCancel";
-            this.ButtonCancel.Size = new System.Drawing.Size(119, 33);
+            this.ButtonCancel.Size = new System.Drawing.Size(89, 27);
             this.ButtonCancel.TabIndex = 3;
             this.ButtonCancel.Text = "Close";
             this.ButtonCancel.UseVisualStyleBackColor = true;
@@ -105,47 +111,43 @@
             // 
             this.MetaDataTablePanel.SetColumnSpan(this.SettingsGroup1Panel, 2);
             this.SettingsGroup1Panel.Controls.Add(this.metaDataView);
-            this.SettingsGroup1Panel.Controls.Add(this.label2);
             this.SettingsGroup1Panel.Controls.Add(this.label1);
-            this.SettingsGroup1Panel.Location = new System.Drawing.Point(11, 66);
+            this.SettingsGroup1Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SettingsGroup1Panel.Location = new System.Drawing.Point(10, 70);
+            this.SettingsGroup1Panel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SettingsGroup1Panel.Name = "SettingsGroup1Panel";
-            this.SettingsGroup1Panel.Size = new System.Drawing.Size(666, 194);
+            this.SettingsGroup1Panel.Size = new System.Drawing.Size(643, 435);
             this.SettingsGroup1Panel.TabIndex = 4;
             // 
-            // label2
+            // metaDataView
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 219);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(402, 32);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "One Key per Line\r\nFor setting a default value for given key use <key> = <value> f" +
-    "ormat";
+            this.metaDataView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metaDataView.Location = new System.Drawing.Point(0, 0);
+            this.metaDataView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metaDataView.MinimumSize = new System.Drawing.Size(15, 16);
+            this.metaDataView.Name = "metaDataView";
+            this.metaDataView.Size = new System.Drawing.Size(643, 435);
+            this.metaDataView.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Location = new System.Drawing.Point(2, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "XML";
             // 
-            // metaDataView
-            // 
-            this.metaDataView.Location = new System.Drawing.Point(6, 26);
-            this.metaDataView.MinimumSize = new System.Drawing.Size(20, 20);
-            this.metaDataView.Name = "metaDataView";
-            this.metaDataView.Size = new System.Drawing.Size(650, 165);
-            this.metaDataView.TabIndex = 3;
-            // 
             // MetaDataForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 341);
+            this.ClientSize = new System.Drawing.Size(655, 558);
             this.Controls.Add(this.MetaDataTablePanel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MetaDataForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "MetaDataForm";
             this.MetaDataTablePanel.ResumeLayout(false);
             this.HeaderPanel.ResumeLayout(false);
@@ -165,7 +167,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button ButtonCancel;
         private System.Windows.Forms.Panel SettingsGroup1Panel;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.WebBrowser metaDataView;
     }
