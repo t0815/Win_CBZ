30b9066bcb64a8bf93146d494ff2b7a63d6a323a
t0815 <10379828+t0815@users.noreply.github.com>
2023-08-21 19:06:02 +0200

    build new release

--- MyCBZ/Win_CBZSettings.Designer.cs
+++ MyCBZ/Win_CBZSettings.Designer.cs
@@ -37,7 +37,7 @@ namespace Win_CBZ {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.10.37b")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.11.38b")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
--- Readme.md
+++ Readme.md
@@ -29,7 +29,7 @@ Microsoft Visual Studio [Community] 2022 (64-Bit)
 
 ## Latest Release
 
-v0.10.37b released!
+v0.11.38b released!
 
 ## Third party components
 
