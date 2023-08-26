7c8e93dd611b1ff9ea6c69a399dee0c24a8f61c1
t0815 <10379828+t0815@users.noreply.github.com>
2023-08-26 20:02:21 +0200

    Fix version numbers in assembly Add actual x64 build

--- MyCBZ/Properties/AssemblyInfo.cs
+++ MyCBZ/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Trash s0Ft")]
 [assembly: AssemblyProduct("Win_CBZ")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2022-2032")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.7.0")]
-[assembly: AssemblyFileVersion("0.7.7.0")]
+[assembly: AssemblyVersion("0.11.44.0")]
+[assembly: AssemblyFileVersion("0.11.44.0")]
+[assembly: NeutralResourcesLanguage("en")]
