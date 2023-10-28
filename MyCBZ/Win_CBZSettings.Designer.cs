c7490ec395f2c9eb6c4d99fb72f7dce3e84dc190
t0815 <10379828+t0815@users.noreply.github.com>
2023-10-28 07:33:02 +0200

    bump ver

--- MyCBZ/Win_CBZSettings.Designer.cs
+++ MyCBZ/Win_CBZSettings.Designer.cs
@@ -37,7 +37,7 @@ namespace Win_CBZ {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.13.56b")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.13.57b")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
