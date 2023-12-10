40dd3c47461a6517b9ece20c34b417bde3d34d3d
t0815 <10379828+t0815@users.noreply.github.com>
2023-12-10 23:19:23 +0100

    update image method impl.

--- MyCBZ/Models/Page.cs
+++ MyCBZ/Models/Page.cs
@@ -723,14 +723,17 @@ namespace Win_CBZ
                 Size = ImageFileInfo.Length;
                 
                 Compressed = false;
+                Changed = true;
                 LastModified = LocalFile.LastModified;
                 Name = LocalFile.FileName;
                 Key = RandomId.getInstance().make();
 
+                TemporaryFileId = RandomId.getInstance().make();
                 Format = new PageImageFormat();
                 Image = null;
                 ImageLoaded = false;
                 ImageStream = imageStream;
+                ThumbnailInvalidated = true;
                 
 
             }
