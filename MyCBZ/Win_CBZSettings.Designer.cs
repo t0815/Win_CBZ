43bc6dff6d072aa12dac0e6e6e9c610b68fa5a6d
t0815 <10379828+t0815@users.noreply.github.com>
2023-12-14 00:21:44 +0100

    update settings

--- MyCBZ/Win_CBZSettings.Designer.cs
+++ MyCBZ/Win_CBZSettings.Designer.cs
@@ -37,7 +37,7 @@ namespace Win_CBZ {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.16.98b")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.16.99b")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
