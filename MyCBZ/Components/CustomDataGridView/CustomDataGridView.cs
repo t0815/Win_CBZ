c17288cdea4754eed11bb0744c56c21089f82520
t0815 <10379828+t0815@users.noreply.github.com>
2024-02-10 15:34:35 +0100

    fix code format

--- MyCBZ/Components/CustomDataGridView/CustomDataGridView.cs
+++ MyCBZ/Components/CustomDataGridView/CustomDataGridView.cs
@@ -10,32 +10,32 @@ using Win_CBZ.Extensions;
 
 namespace Win_CBZ.Components.CustomDataGridView
 {
-    public partial class CustomDataGridView : System.Windows.Forms.DataGridView
+    internal partial class CustomDataGridView : System.Windows.Forms.DataGridView
     {
 
         public bool DisableArrowNavigationMode { get; set; }
    
-           public CustomDataGridView()
-           {
-               DisableArrowNavigationMode = false;
-           }
+        public CustomDataGridView()
+        {
+            DisableArrowNavigationMode = false;
+        }
    
-           protected override bool ProcessDialogKey(Keys keyData)
-           {
-               if (DisableArrowNavigationMode)
-               if (EditingControl != null)
-                   if (keyData == Keys.Enter)
-                       return false;
-               return base.ProcessDialogKey(keyData);
-           }
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (DisableArrowNavigationMode)
+            if (EditingControl != null)
+                if (keyData == Keys.Enter)
+                    return false;
+            return base.ProcessDialogKey(keyData);
+        }
 
-           protected override bool ProcessDataGridViewKey(KeyEventArgs e)
-           {
-                 if (DisableArrowNavigationMode)
-                     if (EditingControl != null)
-                      if (e.KeyCode == Keys.Down || e.KeyCode == Keys.Up || e.KeyCode == Keys.Enter)
-                          return false;
-                  return base.ProcessDataGridViewKey(e);
-              }
+        protected override bool ProcessDataGridViewKey(KeyEventArgs e)
+        {
+                if (DisableArrowNavigationMode)
+                    if (EditingControl != null)
+                    if (e.KeyCode == Keys.Down || e.KeyCode == Keys.Up || e.KeyCode == Keys.Enter)
+                        return false;
+                return base.ProcessDataGridViewKey(e);
+        }
     }
 }
