f86528073065e597f8997bfa5ccc1b567f9d2e86
t0815 <10379828+t0815@users.noreply.github.com>
2024-03-10 21:28:20 +0100

    fix index version detection logic

--- MyCBZ/Helper/MetaDataVersionFlavorHandler.cs
+++ MyCBZ/Helper/MetaDataVersionFlavorHandler.cs
@@ -36,12 +36,20 @@ namespace Win_CBZ.Helper
             return MetaDataVersionFlavorHandler.Instance;
         }
 
+        public MetaDataVersionFlavorHandler SetTargetVersion(PageIndexVersion version)
+        {
+            MetaWriteVersion = version;
+
+            return this;
+        }
+
         public MetaData.PageIndexVersion HandlePageIndexVersion(bool onlyTarget = false)
         {
 
+            MetaVersion = Program.ProjectModel.MetaData.IndexVersionSpecification;
             MetaWriteVersion = TargetVersion();
-            
-            if (MetaVersion != Program.ProjectModel.MetaData.IndexVersionSpecification)
+
+            if (MetaWriteVersion != Program.ProjectModel.MetaData.IndexVersionSpecification)
             {
                 MetaVersion = Program.ProjectModel.MetaData.IndexVersionSpecification;
             }
@@ -56,29 +64,29 @@ namespace Win_CBZ.Helper
             switch (MetaVersionSetting)
             {
                 case 1:
-                    MetaVersion = PageIndexVersion.VERSION_1;
+                    MetaWriteVersion = PageIndexVersion.VERSION_1;
                     break;
                 case 2:
-                    MetaVersion = PageIndexVersion.VERSION_2;
+                    MetaWriteVersion = PageIndexVersion.VERSION_2;
                     break;
                 default:
-                    MetaVersion = PageIndexVersion.VERSION_1;
+                    MetaWriteVersion = PageIndexVersion.VERSION_1;
                     break;
             }
 
-            return MetaVersion;
+            return MetaWriteVersion;
         }
 
         public MetaDataEntryPage CreateIndexEntry(Page page)
         {
             MetaDataEntryPage newPageEntry = new MetaDataEntryPage();
 
-            if (MetaVersion.HasFlag(PageIndexVersion.VERSION_2))
+            if (MetaWriteVersion.HasFlag(PageIndexVersion.VERSION_2))
             {
                 newPageEntry.SetAttribute(MetaDataEntryPage.COMIC_PAGE_ATTRIBUTE_IMAGE, page.Name)
                     .SetAttribute(MetaDataEntryPage.COMIC_PAGE_ATTRIBUTE_KEY, page.Key);
             }
-            else if (MetaVersion.HasFlag(PageIndexVersion.VERSION_1))
+            else if (MetaWriteVersion.HasFlag(PageIndexVersion.VERSION_1))
             {
                 newPageEntry.SetAttribute(MetaDataEntryPage.COMIC_PAGE_ATTRIBUTE_IMAGE, page.Number.ToString())
                     .SetAttribute(MetaDataEntryPage.COMIC_PAGE_ATTRIBUTE_KEY, page.Name);
