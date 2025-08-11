d6f21c88d0f82daa5fe2b03bdc856a1cb18daa23
t0815 <10379828+t0815@users.noreply.github.com>
2025-08-11 19:33:26 +0200

    Add draft for page-filter form....

--- MyCBZ/Forms/PageFilterForm.Designer.cs
+++ MyCBZ/Forms/PageFilterForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Win_CBZ.Forms
 {
-    partial class PageRangeSelectionForm
+    partial class PageFilterForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageRangeSelectionForm));
             ItemEditorTableLayout = new System.Windows.Forms.TableLayoutPanel();
             label2 = new System.Windows.Forms.Label();
             CancelBtn = new System.Windows.Forms.Button();
@@ -39,48 +37,60 @@
             HeaderPicture = new System.Windows.Forms.PictureBox();
             TextBoxSelections = new System.Windows.Forms.TextBox();
             label1 = new System.Windows.Forms.Label();
-            TextBoxOffset = new System.Windows.Forms.TextBox();
-            CheckBoxUseOffset = new System.Windows.Forms.CheckBox();
             pictureBox2 = new System.Windows.Forms.PictureBox();
-            ValidationErrorProvider = new System.Windows.Forms.ErrorProvider(components);
-            RangeSelectionTooltip = new System.Windows.Forms.ToolTip(components);
+            CheckBoxUseOffset = new System.Windows.Forms.CheckBox();
+            TextBoxOffset = new System.Windows.Forms.TextBox();
+            checkBox1 = new System.Windows.Forms.CheckBox();
+            comboBox1 = new System.Windows.Forms.ComboBox();
+            maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            checkBox2 = new System.Windows.Forms.CheckBox();
             ItemEditorTableLayout.SuspendLayout();
             HeaderPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)HeaderPicture).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)ValidationErrorProvider).BeginInit();
             SuspendLayout();
             // 
             // ItemEditorTableLayout
             // 
-            ItemEditorTableLayout.ColumnCount = 4;
+            ItemEditorTableLayout.ColumnCount = 6;
             ItemEditorTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             ItemEditorTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 110F));
             ItemEditorTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 110F));
             ItemEditorTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            ItemEditorTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 110F));
+            ItemEditorTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             ItemEditorTableLayout.Controls.Add(label2, 2, 1);
-            ItemEditorTableLayout.Controls.Add(CancelBtn, 3, 6);
-            ItemEditorTableLayout.Controls.Add(OkButton, 2, 6);
             ItemEditorTableLayout.Controls.Add(HeaderPanel, 0, 0);
             ItemEditorTableLayout.Controls.Add(TextBoxSelections, 1, 2);
             ItemEditorTableLayout.Controls.Add(label1, 1, 1);
-            ItemEditorTableLayout.Controls.Add(pictureBox2, 1, 6);
             ItemEditorTableLayout.Controls.Add(CheckBoxUseOffset, 1, 3);
             ItemEditorTableLayout.Controls.Add(TextBoxOffset, 2, 3);
+            ItemEditorTableLayout.Controls.Add(checkBox1, 1, 4);
+            ItemEditorTableLayout.Controls.Add(comboBox1, 2, 4);
+            ItemEditorTableLayout.Controls.Add(OkButton, 3, 9);
+            ItemEditorTableLayout.Controls.Add(CancelBtn, 4, 9);
+            ItemEditorTableLayout.Controls.Add(pictureBox2, 1, 9);
+            ItemEditorTableLayout.Controls.Add(maskedTextBox1, 2, 6);
+            ItemEditorTableLayout.Controls.Add(maskedTextBox2, 4, 6);
+            ItemEditorTableLayout.Controls.Add(checkBox2, 1, 6);
             ItemEditorTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             ItemEditorTableLayout.Location = new System.Drawing.Point(0, 0);
             ItemEditorTableLayout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             ItemEditorTableLayout.Name = "ItemEditorTableLayout";
-            ItemEditorTableLayout.RowCount = 7;
+            ItemEditorTableLayout.RowCount = 10;
             ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 59F));
             ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
             ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
             ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
-            ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
-            ItemEditorTableLayout.Size = new System.Drawing.Size(367, 230);
-            ItemEditorTableLayout.TabIndex = 2;
+            ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 61F));
+            ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            ItemEditorTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            ItemEditorTableLayout.Size = new System.Drawing.Size(481, 450);
+            ItemEditorTableLayout.TabIndex = 3;
             // 
             // label2
             // 
@@ -95,7 +105,7 @@
             // 
             CancelBtn.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
             CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            CancelBtn.Location = new System.Drawing.Point(264, 184);
+            CancelBtn.Location = new System.Drawing.Point(359, 343);
             CancelBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             CancelBtn.Name = "CancelBtn";
             CancelBtn.Size = new System.Drawing.Size(99, 35);
@@ -107,25 +117,25 @@
             // 
             OkButton.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
             OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            OkButton.Location = new System.Drawing.Point(136, 184);
+            OkButton.Location = new System.Drawing.Point(248, 343);
             OkButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             OkButton.Name = "OkButton";
             OkButton.Size = new System.Drawing.Size(100, 35);
             OkButton.TabIndex = 3;
             OkButton.Text = "Ok";
             OkButton.UseVisualStyleBackColor = true;
-            OkButton.Click += OkButton_Click;
             // 
             // HeaderPanel
             // 
+            HeaderPanel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             HeaderPanel.BackColor = System.Drawing.Color.White;
-            ItemEditorTableLayout.SetColumnSpan(HeaderPanel, 4);
+            ItemEditorTableLayout.SetColumnSpan(HeaderPanel, 6);
             HeaderPanel.Controls.Add(HeaderLabel);
             HeaderPanel.Controls.Add(HeaderPicture);
             HeaderPanel.Location = new System.Drawing.Point(3, 0);
             HeaderPanel.Margin = new System.Windows.Forms.Padding(3, 0, 3, 2);
             HeaderPanel.Name = "HeaderPanel";
-            HeaderPanel.Size = new System.Drawing.Size(361, 57);
+            HeaderPanel.Size = new System.Drawing.Size(476, 57);
             HeaderPanel.TabIndex = 2;
             // 
             // HeaderLabel
@@ -134,9 +144,9 @@
             HeaderLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
             HeaderLabel.Location = new System.Drawing.Point(97, 11);
             HeaderLabel.Name = "HeaderLabel";
-            HeaderLabel.Size = new System.Drawing.Size(131, 28);
+            HeaderLabel.Size = new System.Drawing.Size(111, 28);
             HeaderLabel.TabIndex = 1;
-            HeaderLabel.Text = "Select Pages...";
+            HeaderLabel.Text = "Filter Pages";
             // 
             // HeaderPicture
             // 
@@ -153,10 +163,9 @@
             // TextBoxSelections
             // 
             ItemEditorTableLayout.SetColumnSpan(TextBoxSelections, 2);
-            ValidationErrorProvider.SetIconPadding(TextBoxSelections, -15);
             TextBoxSelections.Location = new System.Drawing.Point(23, 94);
             TextBoxSelections.Name = "TextBoxSelections";
-            TextBoxSelections.PlaceholderText = " 3;1-10;1,2,3";
+            TextBoxSelections.PlaceholderText = "Name, CRC32";
             TextBoxSelections.Size = new System.Drawing.Size(214, 27);
             TextBoxSelections.TabIndex = 7;
             // 
@@ -166,17 +175,21 @@
             label1.AutoSize = true;
             label1.Location = new System.Drawing.Point(23, 71);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(101, 20);
+            label1.Size = new System.Drawing.Size(53, 20);
             label1.TabIndex = 8;
-            label1.Text = "Select Page(s)";
+            label1.Text = "Search";
             // 
-            // TextBoxOffset
+            // pictureBox2
             // 
-            TextBoxOffset.Location = new System.Drawing.Point(133, 135);
-            TextBoxOffset.Name = "TextBoxOffset";
-            TextBoxOffset.PlaceholderText = "0";
-            TextBoxOffset.Size = new System.Drawing.Size(104, 27);
-            TextBoxOffset.TabIndex = 11;
+            pictureBox2.Image = Properties.Resources.information;
+            pictureBox2.InitialImage = Properties.Resources.information;
+            pictureBox2.Location = new System.Drawing.Point(26, 338);
+            pictureBox2.Margin = new System.Windows.Forms.Padding(6, 0, 0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new System.Drawing.Size(36, 43);
+            pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            pictureBox2.TabIndex = 32;
+            pictureBox2.TabStop = false;
             // 
             // CheckBoxUseOffset
             // 
@@ -189,52 +202,70 @@
             CheckBoxUseOffset.Text = "Offset";
             CheckBoxUseOffset.UseVisualStyleBackColor = true;
             // 
-            // pictureBox2
+            // TextBoxOffset
             // 
-            pictureBox2.Image = Properties.Resources.information;
-            pictureBox2.InitialImage = Properties.Resources.information;
-            pictureBox2.Location = new System.Drawing.Point(26, 179);
-            pictureBox2.Margin = new System.Windows.Forms.Padding(6, 0, 0, 0);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new System.Drawing.Size(36, 43);
-            pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            pictureBox2.TabIndex = 32;
-            pictureBox2.TabStop = false;
-            RangeSelectionTooltip.SetToolTip(pictureBox2, resources.GetString("pictureBox2.ToolTip"));
+            TextBoxOffset.Location = new System.Drawing.Point(133, 135);
+            TextBoxOffset.Name = "TextBoxOffset";
+            TextBoxOffset.PlaceholderText = "0";
+            TextBoxOffset.Size = new System.Drawing.Size(104, 27);
+            TextBoxOffset.TabIndex = 11;
             // 
-            // ValidationErrorProvider
+            // checkBox1
             // 
-            ValidationErrorProvider.ContainerControl = this;
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new System.Drawing.Point(23, 176);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new System.Drawing.Size(71, 24);
+            checkBox1.TabIndex = 33;
+            checkBox1.Text = "Status";
+            checkBox1.UseVisualStyleBackColor = true;
             // 
-            // RangeSelectionTooltip
+            // comboBox1
             // 
-            RangeSelectionTooltip.AutoPopDelay = 600000;
-            RangeSelectionTooltip.InitialDelay = 500;
-            RangeSelectionTooltip.IsBalloon = true;
-            RangeSelectionTooltip.ReshowDelay = 100;
-            RangeSelectionTooltip.ToolTipTitle = "Win_CBZ";
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new System.Drawing.Point(133, 176);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new System.Drawing.Size(104, 28);
+            comboBox1.TabIndex = 34;
             // 
-            // PageRangeSelectionForm
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new System.Drawing.Point(133, 212);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new System.Drawing.Size(104, 27);
+            maskedTextBox1.TabIndex = 35;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new System.Drawing.Point(355, 212);
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new System.Drawing.Size(104, 27);
+            maskedTextBox2.TabIndex = 36;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new System.Drawing.Point(23, 212);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new System.Drawing.Size(58, 24);
+            checkBox2.TabIndex = 37;
+            checkBox2.Text = "Size";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // PageFilterForm
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(367, 230);
+            ClientSize = new System.Drawing.Size(481, 450);
             Controls.Add(ItemEditorTableLayout);
-            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            KeyPreview = true;
-            Name = "PageRangeSelectionForm";
-            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            Text = "Select Range";
-            FormClosing += PageRangeSelectionForm_FormClosing;
-            Load += PageRangeSelectionForm_Load;
-            KeyUp += PageRangeSelectionForm_KeyUp;
+            Name = "PageFilterForm";
+            Text = "PageFilterForm";
             ItemEditorTableLayout.ResumeLayout(false);
             ItemEditorTableLayout.PerformLayout();
             HeaderPanel.ResumeLayout(false);
             HeaderPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)HeaderPicture).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)ValidationErrorProvider).EndInit();
             ResumeLayout(false);
         }
 
@@ -242,17 +273,20 @@
 
         private System.Windows.Forms.TableLayoutPanel ItemEditorTableLayout;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button CancelBtn;
-        private System.Windows.Forms.Button OkButton;
         private System.Windows.Forms.Panel HeaderPanel;
         private System.Windows.Forms.Label HeaderLabel;
         private System.Windows.Forms.PictureBox HeaderPicture;
         private System.Windows.Forms.TextBox TextBoxSelections;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox TextBoxOffset;
-        private System.Windows.Forms.CheckBox CheckBoxUseOffset;
-        private System.Windows.Forms.ErrorProvider ValidationErrorProvider;
-        private System.Windows.Forms.ToolTip RangeSelectionTooltip;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox CheckBoxUseOffset;
+        private System.Windows.Forms.TextBox TextBoxOffset;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
